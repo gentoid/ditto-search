175bb57b4205ccc9f3eab31efd10878648ad027f
Viktor Lazarev <taurus101v@gmail.com>
2014-02-20 01:07:43 +0400

    Playing with FS and seesaw

--- src/ditto_search/core.clj
+++ src/ditto_search/core.clj
@@ -1,6 +1,21 @@
-(ns ditto-search.core)
+(ns ditto-search.core
+  (:use seesaw.core
+        me.raynes.fs))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def program-version
+  "0.0.1")
+
+(defn content
+  ([] (content "/"))
+  ([path] (listbox :model (sort (list-dir path)))))
+
+;(listen content
+;        :double (fn [e] (content (selection e))))
+
+(defn -main [& args]
+  (invoke-later
+    (-> (frame :title (str "Ditto Search " program-version),
+               :content (content),
+               :on-close :exit)
+        pack!
+        show!)))
